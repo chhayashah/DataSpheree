17eeee5f5d090d9726d19034290a814d78e26233
chhaya shah <chhayashah9754@gmail.com>
2026-07-11 14:20:18 +0530

    update app.js

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -9,6 +9,22 @@ const rateLimiter = rateLimit({
   },
   standardHeaders: true,
   legacyHeaders: false,
+  validate: { xForwardedForHeader: false },
 });
 
 module.exports = rateLimiter;
+
+// const rateLimit = require("express-rate-limit");
+
+// const rateLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100,
+//   message: {
+//     success: false,
+//     message: "Too many requests, please try again after 15 minutes",
+//   },
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
+
+// module.exports = rateLimiter;
